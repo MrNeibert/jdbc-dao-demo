c94d81295d45d3145838b5477ddb7a9bb291e998
Willian <willian.neibertz@gmail.com>
2023-09-22 11:04:05 -0300

    Department entity Class

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # jdbc-dao-demo
 
-This is a small demo Excercise for data access object in JDBC, for a Udemy course for Java.
+This is a small demo Exercise for data access object in JDBC, for a Udemy course for Java.
--- src/application/Program.java
+++ src/application/Program.java
@@ -1,50 +1,14 @@
 package application;
 
-import db.DB;
-import db.DbException;
 
-import java.sql.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
+import model.entities.Department;
 
 public class Program {
     public static void main(String[] args) {
+        Department obj = new Department(1,"Books");
 
-        Connection conn = null;
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
-        Statement st = null;
+        System.out.println(obj);
 
-        try{
-            conn = DB.getConnection();
-
-            conn.setAutoCommit(false);
-
-            st = conn.createStatement();
-
-            int rowsAffected1 = st.executeUpdate("UPDATE seller SET BaseSalary = 2090 WHERE DepartmentId = 1");
-            
-
-            int rowsAffected2 = st.executeUpdate("UPDATE seller SET BaseSalary = 3090 WHERE DepartmentId = 2");
-
-            System.out.println("Done! Rows affected: " + rowsAffected1 + rowsAffected2);
-
-            conn.commit();
-        }
-        catch (SQLException  e) {
-
-            try {
-                conn.rollback();
-                throw new DbException("Transaction got rolled back! Cause: " + e.getMessage());
-            } catch (SQLException ex) {
-                throw new DbException("Error trying to roll back! Cause: " + ex.getMessage());
-            }
-
-        }
-        finally {
-            DB.closeStatement(st);
-            DB.closeConnection();
-
-        }
 
     }
 }
