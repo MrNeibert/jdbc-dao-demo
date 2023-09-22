1a610f92139e201badbeebe267e691c17088cda5
Willian <willian.neibertz@gmail.com>
2023-09-22 11:12:15 -0300

    Seller entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,12 +2,17 @@ package application;
 
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
     public static void main(String[] args) {
         Department obj = new Department(1,"Books");
 
-        System.out.println(obj);
+        Seller seller = new Seller(21,"Bob","Bob@gmail.com",new Date(),3000.00,obj);
+
+        System.out.println(seller);
 
 
     }
